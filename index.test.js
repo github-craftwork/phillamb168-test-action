eb72a81e5a7da317d5db1c584743b6626f5d062a
Phillip Lamb <Phillip.lamb@nc.gov>
2019-10-14 11:52:04 -0400

    trying a new test

--- index.test.js
+++ index.test.js
@@ -1,5 +1,11 @@
+// index.test.js
 const weekNum = require('./index.js');
 
 test('that weekNum returns a value', () => {
   expect(weekNum).not.toBeNull();
+});
+
+// new test added
+test('that weekNum returns a number', () => {
+  expect(weekNum).toBeDefined();
 });
